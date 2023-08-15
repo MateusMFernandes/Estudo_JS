99166872de238301864c58e7e9dab55865383531
Mateus <mateusmf16@gmail.com>
2023-08-15 20:13:03 -0300

    jogo

--- Exercicio modelo/Ex14/teste.js
+++ Exercicio modelo/Ex14/teste.js
@@ -1,19 +1,21 @@
 
-var inicio = 5
+var inicio = 50
 var fim = 12
-var passos = 0
+var passos = 2
 
-if (inicio<fim) {
-    while (inicio < fim) {
+if (inicio<fim && passos > 0) {
+    while (inicio <= fim) {
         console.log(inicio)
         var inicio = inicio+passos
     }
 } else if (inicio>fim) {
     console.log("[Erro] o fim não pode ser maior que o começo")
-} else if (passos < 1) {
+} else if(passos === 0) {
     console.log("Meu passo não pode ser 0, considerando isso meu passo sera marcado com 1")
-    for (var um = 1; um < fim; um++) {     /**FINALIZAR AINDA  */
+    while (inicio<=fim) {
         console.log(inicio)
+        inicio++
+
     }
 }
 
